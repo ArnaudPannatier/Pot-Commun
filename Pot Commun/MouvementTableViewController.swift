6535237ba436fce2afde508fc6bceaa9e7ae391d
Arnaud Pannatier <arn.pannatier@gmail.com>
2019-02-19 13:10:35 +0100

    Formatting.

--- Pot Commun/MouvementTableViewController.swift
+++ Pot Commun/MouvementTableViewController.swift
@@ -10,33 +10,23 @@ import UIKit
 import CoreData
 
 class MouvementTableViewController: CoreDataTableViewController {
-
-    
     // MARK: MODEL
-    
     var managedObjectContext: NSManagedObjectContext? {
         didSet {
             updateUI()
         }
     }
-
-    
-    
-    
     fileprivate func updateUI() {
-        
+
         if let context = managedObjectContext {
             let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Mouvement")
             request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
             fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
-        }else{
+        } else {
             fetchedResultsController = nil
         }
     }
-    
-    
-    
-     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         managedObjectContext = (UIApplication.shared.delegate as? AppDelegate)?.managedObjectContext
@@ -47,94 +37,25 @@ class MouvementTableViewController: CoreDataTableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-     
-     /*
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-*/
-  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MouvementCell", for: indexPath)
 
         if let mouvement = fetchedResultsController?.object(at: indexPath) as? Mouvement {
             var cellText: String?
             var detailText: String?
-            
-            mouvement.managedObjectContext?.performAndWait{
+
+            mouvement.managedObjectContext?.performAndWait {
                 let dateform = DateFormatter()
                 dateform.dateFormat = "dd MMMM YYYY"
                 dateform.locale = Locale(identifier: "fr_FR")
-                detailText = mouvement.valeur!.stringValue + " francs - " + dateform.string(from:mouvement.date!)
+                detailText = mouvement.valeur!.stringValue + " francs - " + dateform.string(from: mouvement.date!)
                 cellText = String(mouvement.coloc!)
-                
+
             }
             cell.textLabel?.text = cellText!
             cell.detailTextLabel?.text = detailText!
-            
-            
         }
 
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
